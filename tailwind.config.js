4afb6d9adfb2071448e7f1c13a18272d4be561cd
Gusti Panji Widodo <gustipanji2006@gmail.com>
2025-02-16 23:22:07 +0700

    update to html code

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './index.html', // Add paths to all your template files
+    ],
+    theme: {
+      extend: {
+        colors: {
+          'custom-green': '#07C85E',
+          'custom-dark': '#08160D',
+        },
+      },
+    },
+    plugins: [],
+  }
